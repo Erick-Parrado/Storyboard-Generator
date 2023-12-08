ba0dccc1d73817f959851c05ff5610c2fb17f3aa
Sarita A. Salazar <109240008+ArsenicGirl@users.noreply.github.com>
2023-12-07 23:57:16 -0500

    Finalización de creación y modelamiento de pantallas

--- app/src/main/java/com/example/storyboard_generator/EditUserForm.java
+++ app/src/main/java/com/example/storyboard_generator/EditUserForm.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class EditUserProfile extends AppCompatActivity {
+public class EditUserForm extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_user_profile);
+        setContentView(R.layout.activity_edit_user_form);
     }
 }
