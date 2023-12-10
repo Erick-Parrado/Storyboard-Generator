92b669ad994aaaddfdd27dadfbd6732f56855a3a
Karen Soler <kjsoler19@misena.edu.co>
2023-12-10 09:38:04 -0500

    Navigation v1

--- app/src/main/java/com/example/storyboard_generator/Login.java
+++ app/src/main/java/com/example/storyboard_generator/Login.java
@@ -2,13 +2,35 @@ package com.example.storyboard_generator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    private Button btnLoginLogin;
+    private Button btnRegisterLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        this.btnLoginLogin = findViewById(R.id.btnLoginLogin);
+        this.btnRegisterLogin = findViewById(R.id.btnRegisterLogin);
+
+        btnLoginLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent goProjects = new Intent(getApplicationContext(), Projects.class);
+                startActivity(goProjects);
+            }
+        });
+        btnRegisterLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goProjects = new Intent(getApplicationContext(), Register.class);
+                startActivity(goProjects);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/storyboard_generator/Projects.java
+++ app/src/main/java/com/example/storyboard_generator/Projects.java
@@ -2,13 +2,19 @@ package com.example.storyboard_generator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.PopupMenu;
 
 import com.example.storyboard_generator.listview.ListAdapter;
 import com.example.storyboard_generator.listview.Model;
@@ -17,27 +23,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Projects extends AppCompatActivity{
-/*
-    private ListView listView;
-
-    private List<Model> lista = new ArrayList<>();
-    ListAdapter adapter = new ListAdapter(Projects.this,R.layout.activity_projects);
-
-*/
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceSate) {
+        super.onCreate(savedInstanceSate);
         setContentView(R.layout.activity_projects);
-      /*  begin();
-       lista.add(new Model(R.mipmap.ic_launcher, "Proyecto1","Ghibli","hace 12 seg"));
 
-        listView.setAdapter(adapter);*/
+        ArrayList<String> projects = new ArrayList<>();
+        projects.add("Pixel Art");
+        projects.add("Teravision Games");
+        projects.add("Tan grande y jugando");
+
+        ListView lvLista = findViewById(R.id.lvLista);
+
+        ArrayAdapter<String> itemsAdpater = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, projects);
+
+        lvLista.setAdapter(itemsAdpater);
     }
 
-/*
-    private void begin(){
-    this.listView = findViewById(R.id.listView);
-    }
-*/
+        /*Menú*/
+        public boolean onCreateOptionsMenu(Menu menu) {
+            super.onCreateOptionsMenu(menu);
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menu1, menu);
+            return true;
+        }
 
+        public void showPopup(View view){
+            PopupMenu popup = new PopupMenu(this, view);
+            MenuInflater inflater = popup.getMenuInflater();
+            inflater.inflate(R.menu.menu1, popup.getMenu());
+            popup.show();
+        }
 }
