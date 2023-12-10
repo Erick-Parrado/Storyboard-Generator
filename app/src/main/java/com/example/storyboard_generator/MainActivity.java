ba613a4304759c9b8af9eb1035a8b3e3d8cabe6d
Karen Soler <kjsoler19@misena.edu.co>
2023-12-10 09:38:04 -0500

    Navigation v1

--- app/src/main/java/com/example/storyboard_generator/MainActivity.java
+++ app/src/main/java/com/example/storyboard_generator/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.storyboard_generator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+    private Button btnLogin;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.btnLogin = findViewById(R.id.btnLogin);
+        this.btnRegister = findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go = new Intent(getApplicationContext(), Login.class);
+                startActivity(go);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go = new Intent(getApplicationContext(), Register.class);
+                startActivity(go);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/storyboard_generator/Projects.java
+++ app/src/main/java/com/example/storyboard_generator/Projects.java
@@ -1,54 +1,57 @@
 package com.example.storyboard_generator;
 
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import com.example.storyboard_generator.databinding.ActivityProjectsBinding;
-//import com.example.storyboard_generator.listview.ListAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+
+import com.example.storyboard_generator.listview.ListAdapter;
+import com.example.storyboard_generator.listview.Model;
 
 import java.util.ArrayList;
+import java.util.List;
 
-
-public class Projects extends AppCompatActivity {
-    ActivityProjectsBinding binding;
-    ListAdapterProjects listAdapter;
-    ArrayList<ListDataProjects> dataArrayList = new ArrayList<>();
-    ListDataProjects listDataProjects;
+public class Projects extends AppCompatActivity{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceSate) {
+        super.onCreate(savedInstanceSate);
+        setContentView(R.layout.activity_projects);
 
-        binding = ActivityProjectsBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        ArrayList<String> projects = new ArrayList<>();
+        projects.add("Pixel Art");
+        projects.add("Teravision Games");
+        projects.add("Tan grande y jugando");
 
-        int[] imageList = {R.drawable.imggallery, R.drawable.logo, R.drawable.uniempresarial};
-        String[] titleList = {"El viaje de Chihiro", "Young Justice","Kimetsu No Yaiba"};
-        String[] studioList = {"Ghibli", "CN", "Uffotable"};
+        ListView lvLista = findViewById(R.id.lvLista);
 
-        for (int i = 0; i < imageList.length; i++){
-            listDataProjects = new ListDataProjects(titleList[i], studioList[i], imageList[i]);
-            dataArrayList.add(listDataProjects);
-        }
-        listAdapter = new ListAdapterProjects(Projects.this, dataArrayList);
-        binding.listView.setAdapter(listAdapter);
-        binding.listView.setClickable(true);
+        ArrayAdapter<String> itemsAdpater = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, projects);
 
-        for (int i = 0; i < listAdapter.getCount(); i++) {
-            View view = listAdapter.getView(i, null, null);
-            final int finalI = i;
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(Projects.this, ProjectTemplate.class);
-                    intent.putExtra("title", titleList[finalI]);
-                    intent.putExtra("studio", studioList[finalI]);
-                    intent.putExtra("image", imageList[finalI]);
-                    startActivity(intent);
-                }
-            });
-        }
+        lvLista.setAdapter(itemsAdpater);
     }
+
+        /*Menú*/
+        public boolean onCreateOptionsMenu(Menu menu) {
+            super.onCreateOptionsMenu(menu);
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.menu1, menu);
+            return true;
+        }
+
+        public void showPopup(View view){
+            PopupMenu popup = new PopupMenu(this, view);
+            MenuInflater inflater = popup.getMenuInflater();
+            inflater.inflate(R.menu.menu1, popup.getMenu());
+            popup.show();
+        }
 }
