31927e808441f8f6bbe8855c4dc6412c2462ee8d
Karen Soler <kjsoler19@misena.edu.co>
2023-12-11 16:26:22 -0500

    Navigation v2

--- app/src/main/java/com/example/storyboard_generator/Projects.java
+++ app/src/main/java/com/example/storyboard_generator/Projects.java
@@ -2,32 +2,70 @@ package com.example.storyboard_generator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.PopupMenu;
-
-import com.example.storyboard_generator.listview.ListAdapter;
-import com.example.storyboard_generator.listview.Model;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class Projects extends AppCompatActivity{
+public class Projects extends AppCompatActivity {
+    private ImageButton backArrow;
+    private ImageButton AddProjects;
+    private ImageButton Unirse;
+    private ImageButton perfilUser;
+    private ImageButton Settings;
+
     @Override
     protected void onCreate(Bundle savedInstanceSate) {
         super.onCreate(savedInstanceSate);
         setContentView(R.layout.activity_projects);
+        backArrow = findViewById(R.id.ibPinClose);
+        this.AddProjects = findViewById(R.id.ibAddProjects);
+        this.Unirse = findViewById(R.id.ibUnirse);
+        this.perfilUser = findViewById(R.id.ibUser);
+        this.Settings = findViewById(R.id.ibSettings);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Projects.this, Register.class));
+                startActivity(new Intent(Projects.this, CreateProjectForm.class));
+                startActivity(new Intent(Projects.this, TeamMembers.class));
+            }
+        });
+        AddProjects.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go = new Intent(getApplicationContext(), CreateProjectForm.class);
+                startActivity(go);
+            }
+        });
+        Unirse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent go = new Intent(getApplicationContext(), Pin.class);
+                startActivity(go);
+            }
+        });
+        perfilUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go = new Intent(getApplicationContext(), UserProfile.class);
+                startActivity(go);
+            }
+        });
+        Settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go = new Intent(getApplicationContext(), com.example.storyboard_generator.Settings.class);
+                startActivity(go);
+            }
+        });
 
+
+        /*LIST VIEW*/
         ArrayList<String> projects = new ArrayList<>();
         projects.add("Pixel Art");
         projects.add("Teravision Games");
@@ -40,18 +78,4 @@ public class Projects extends AppCompatActivity{
         lvLista.setAdapter(itemsAdpater);
     }
 
-        /*Menú*/
-        public boolean onCreateOptionsMenu(Menu menu) {
-            super.onCreateOptionsMenu(menu);
-            MenuInflater inflater = getMenuInflater();
-            inflater.inflate(R.menu.menu1, menu);
-            return true;
-        }
-
-        public void showPopup(View view){
-            PopupMenu popup = new PopupMenu(this, view);
-            MenuInflater inflater = popup.getMenuInflater();
-            inflater.inflate(R.menu.menu1, popup.getMenu());
-            popup.show();
-        }
 }
--- app/src/main/java/com/example/storyboard_generator/UserProfile.java
+++ app/src/main/java/com/example/storyboard_generator/UserProfile.java
@@ -2,13 +2,28 @@ package com.example.storyboard_generator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class UserProfile extends AppCompatActivity {
+    private Button editUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+        this.editUser = findViewById(R.id.btnUserProfileEdit);
+
+        editUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go = new Intent(getApplicationContext(), EditUserForm.class);
+                startActivity(go);
+            }
+        });
+
     }
 }
